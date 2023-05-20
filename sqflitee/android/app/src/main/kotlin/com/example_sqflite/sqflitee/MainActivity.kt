9cd024416f038581dedd6f179ba4c126d2f30bad
Kirito <halid.gundz@gmail.com>
2023-05-20 21:31:28 +0200

    Flutter Serisi V5-Sqflite giris

---
+++ sqflitee/android/app/src/main/kotlin/com/example_sqflite/sqflitee/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example_sqflite.sqflitee
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ sqflitee/lib/models/kahraman.dart
@@ -0,0 +1,11 @@
+class Kahraman{
+
+  late int id;
+  late String isim;
+  late String pozisyon;
+  late double gucSeviyesi;
+
+  Kahraman(this.id,this.isim,this.pozisyon, this.gucSeviyesi);
+
+
+}
